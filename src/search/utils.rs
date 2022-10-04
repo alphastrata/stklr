e61192c23b9225473aed5026298ad2a75659e0dd
Jer <alphastrata@gmail.com>
2022-10-05 07:00:50 +0800

    fix: compiling -removed all mpsc

--- src/search/utils.rs
+++ src/search/utils.rs
@@ -38,16 +38,16 @@ pub enum Flavour {
 pub struct RawLine {
     pub line_num: usize, //NOTE: indentionally duplicate data
     pub all_linked: Linked,
-    pub contents_original: String,
+    pub contents: String,
     pub flavour: Flavour,
     pub idents: Vec<String>, //NOTE: expended data (exists temporarily to populate the SourceCode struct(s))
     pub source_file: PathBuf,
 }
 
 #[derive(Default, Debug, Clone, Hash)]
-struct AdjustedLine {
+pub struct AdjustedLine {
     pub line_num: usize,
-    pub contents_original: String,
+    pub contents: String,
     pub source_file: PathBuf,
 }
 
@@ -55,7 +55,7 @@ impl From<RawLine> for AdjustedLine {
     fn from(line: RawLine) -> Self {
         Self {
             line_num: line.line_num,
-            contents_original: line.contents_original,
+            contents: line.contents,
             source_file: line.source_file,
         }
     }
@@ -105,86 +105,33 @@ impl SourceTree {
         .populate_idents()
     }
     /// makes adjustments to RawLines from within RawSourceCode's RawLines
-    fn make_adjustments(&self, tx: Sender<(RawLine, String)>) {
-        let tx_c = tx.clone();
-        self.source_files.iter().for_each(|sf| {
-            sf.named_idents.iter().cloned().for_each(|i| {
-                sf.iter()
-                    .for_each(|(_, raw_line)| match raw_line.flavour {
-                        Flavour::Docstring => {
-                            //BUG: haven't accounted for multiple occurences, or a nested
-                            //occurence.
-                            if raw_line.should_be_modified(&i) {
-                                let mut raw_line = raw_line.clone();
-                                    raw_line.contents_original = raw_line.process_changes(&i);
-
-                                tx_c.send(
-                                    (raw_line.to_owned(),
-                                    sf.file.display().to_string())
-                                )
-                                .expect("Unable to send line on channel, something has gone HORRIBLY WRONG!");
-                            }
-                            //dbg!("SENT!");
-                        }
-                        _ => {},
-                    });
-            });
-        })
+    fn make_adjustments(&self) {
+        self.source_files.iter().for_each(|sf| {})
     }
 
-    pub fn write_changes(self, write_flag: bool) {
-        // Adjustment thread -> sends to builder thread
-        let tsrsc = self.clone();
-        let (tx_adj_2_build, rx_build) = mpsc::channel();
-        let tx_adj_c = tx_adj_2_build.clone();
-        let adjust_t = thread::spawn(move || tsrsc.make_adjustments(tx_adj_c));
+    pub fn write_changes(file: PathBuf, changes: &mut Vec<AdjustedLine>, write_flag: bool) {
+        changes.sort_by(|a, b| a.line_num.cmp(&b.line_num));
+        let output: Vec<String> = changes
+            .into_iter()
+            .map(|adj_line| adj_line.contents.to_owned())
+            .collect();
 
-        // Builder thread -> sends to writer
-        let (tx_build, rx_writer) = mpsc::channel();
-
-        // THE RX_BUILD gets values here, from tsrsc.make_adjustments(tx_c)
-        let builder_t = thread::spawn(move || {
-            let mut sf_hm: HashMap<String, Vec<AdjustedLine>> = HashMap::new();
-            while let Ok((rl, filepath)) = rx_build.recv() {
-                sf_hm.entry(filepath).and_modify(|v| v.push(rl.into()));
-                dbg!("PUSH");
-            }
-            tx_build
-                .send(sf_hm)
-                .expect("unable to send complete sf_hm to writer!");
-            dbg!("SF_HM GONE");
-        });
-
-        if let Ok(_) = adjust_t.join() {
-            drop(tx_adj_2_build);
+        if write_flag {
+            _ = fs::write(&file, output.join("\n")).unwrap(); //FIXME:
+        } else {
+            changes.iter().for_each(|e| println!("{}", e));
         }
+    }
+}
 
-        if let Ok(_) = builder_t.join() {
-            // ALL THE LINES ARE IN SF_HM
-        }
-
-        // THE WRITER thread is actually *this* one, the main one.
-        while let Ok(sf_hm) = rx_writer.recv() {
-            // sf_hm is a hashmap of filepath, adjusted_line
-            dbg!(&sf_hm);
-
-            sf_hm.iter().for_each(|(sf_path, v)| {
-                let mut output: Vec<&str> = Vec::new();
-                for (e, _) in v.iter().enumerate() {
-                    let current = &v[e];
-                    println!("{}::{}", &current.line_num, &current.contents_original);
-
-                    output.push(&current.contents_original)
-                }
-
-                if write_flag {
-                    fs::write(&sf_path, output.join("\n"))
-                        .expect("problem writing output to filepath")
-                } else {
-                    output.iter().for_each(|e| println!("{}", e));
-                }
-            });
-        }
+impl Display for AdjustedLine {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}", &self.line_num, &self.contents)
+    }
+}
+impl Display for RawLine {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}", &self.line_num, &self.contents)
     }
 }
 
@@ -234,7 +181,7 @@ impl RawSourceCode {
                     if let Ok(l) = l {
                         let mut raw_line = RawLine {
                             all_linked: Linked::Unprocessed,
-                            contents_original: l.into(),
+                            contents: l.into(),
                             line_num: e,
                             ..Default::default()
                         };
@@ -254,33 +201,48 @@ impl RawSourceCode {
         raw_source_file.named_idents.retain(|x| x != ""); //TODO: can you initialise this better so avoid this?
         raw_source_file
     }
+
+    fn make_adjustments(&self, idents: &[String]) -> Vec<AdjustedLine> {
+        idents
+            .iter()
+            .flat_map(|id| {
+                self.m
+                    .iter()
+                    .filter(|(_, raw_line)| raw_line.should_be_modified(&id))
+                    .map(|(_, f)| f.clone().process_changes(&id))
+                    .map(|rpl| rpl.clone().into())
+                    .collect::<Vec<AdjustedLine>>()
+            })
+            .collect::<Vec<AdjustedLine>>()
+    }
 }
 
+//-------
 impl RawLine {
     /// Will return true for a SINGLE instance of when a modification should be made, how many may
     /// really be in there is the domain of [`process_changes`]
     fn should_be_modified(&self, i: &str) -> bool {
-        if self.contents_original.contains(i)
-            || self.contents_original.contains(&format!("{i}s"))
-            || self.contents_original.contains(&format!("{i}."))
-            || self.contents_original.contains(&format!("{i}'s"))
-                && !self.contents_original.contains(&format!("`{}`", i))
-                && self.contents_original.contains("///")
+        if matches!(self.flavour, Flavour::Docstring) && self.contents.contains(i)
+            || self.contents.contains(&format!("{i}s"))
+            || self.contents.contains(&format!("{i}."))
+            || self.contents.contains(&format!("{i}'s"))
+                && !self.contents.contains(&format!("`{}`", i))
+                && self.contents.contains("///")
         {
             return true;
         }
         false
     }
     /// Actually processes the modifications to a RawLine's contents_modified
-    fn process_changes(&self, i: &str) -> String {
+    fn process_changes(mut self, i: &str) -> Self {
         // Account for the `matches` call's not handling out-of-bounds
         let padded_i = format!(" {} ", i);
-        let padded_self = format!(" {} ", self.contents_original);
+        let padded_self = format!(" {} ", self.contents);
 
         // Account for abnormal docstring end chars like '.'
         let mut changes_to_make: Vec<&str> = padded_self.matches(&padded_i).collect();
         if changes_to_make.is_empty() {
-            changes_to_make = self.contents_original.matches(i).collect();
+            changes_to_make = self.contents.matches(i).collect();
         }
 
         let needle = &format!("[`{}`]", i);
@@ -290,9 +252,11 @@ impl RawLine {
         //        dbg!(&changes_to_make);
         //        dbg!(&needle);
         //
-        self.contents_original
+        self.contents = self
+            .contents
             .clone()
-            .replacen(i, needle, changes_to_make.len())
+            .replacen(i, needle, changes_to_make.len());
+        self
     }
     fn process(&mut self) {
         self.find_docs();
@@ -301,13 +265,13 @@ impl RawLine {
     }
     //TODO: Make this DRY with a macro
     fn find_idents(&mut self) {
-        let text = self.contents_original.to_owned();
+        let text = self.contents.to_owned();
         macro_rules! generate_ident_find_loops {
             ($($CONST:ident),*) => {
                 $(for caps in $CONST.captures_iter(&text) {
                     if let Some(v) = caps.name("ident") {
                         let cap = v.as_str().to_string();
-                        trace!("{}::{}", self.line_num, cap);
+                        //trace!("{}::{}", self.line_num, cap);
                         self.flavour = Flavour::Declare;
                         self.idents.push(cap);
                     }
@@ -327,7 +291,7 @@ impl RawLine {
 
     /// Process preview_changes RawSourceCode [`find_docs`]
     fn find_docs(&mut self) {
-        let text = self.contents_original.to_owned();
+        let text = self.contents.to_owned();
         for caps in RUST_DOCSTRING.captures_iter(&text) {
             if let Some(_) = caps.name("ident") {
                 self.flavour = Flavour::Docstring;
@@ -353,7 +317,7 @@ mod tests {
                         }"#;
 
         let mut raw_line = RawLine {
-            contents_original: example.into(),
+            contents: example.into(),
             ..Default::default()
         };
         raw_line.process();
@@ -363,7 +327,7 @@ mod tests {
     fn handle_lifetime_annotations() {
         let example = r#"async fn servitude<'a>(a: &'a str) -> bool{}"#;
         let mut raw_line = RawLine {
-            contents_original: example.to_string(),
+            contents: example.to_string(),
             ..Default::default()
         };
         raw_line.process();
@@ -377,7 +341,7 @@ mod tests {
         };
         }"#;
         let mut raw_line = RawLine {
-            contents_original: example.into(),
+            contents: example.into(),
             ..Default::default()
         };
         raw_line.process();
@@ -389,34 +353,35 @@ mod tests {
         let example =
             r#"a preview_changes to be linked, and another preview_changes here linked too."#;
         let raw_line = RawLine {
-            contents_original: example.to_string(),
+            contents: example.to_string(),
             idents: vec!["preview_changes".into()],
             ..Default::default()
         };
         let res = raw_line.process_changes(&raw_line.idents[0]);
         let expected =
             "a [`preview_changes`] to be linked, and another [`preview_changes`] here linked too.";
-        assert_eq!(expected, &res);
+        assert_eq!(expected, &res.contents);
     }
     #[test]
     fn trial_on_this_source() {
         let st = SourceTree::new_from_cwd();
-        st.write_changes(false);
+        _ = st.source_files.iter();
+        //.map(|rsc| SourceTree::write_changes(rsc, false);).collect::<Vec<()>();
     }
 
     #[test]
     fn fullstop_after_ident() {
         let example = r#"a preview_changes to the mighty SourceTree."#;
         let mut raw_line = RawLine {
-            contents_original: example.to_string(),
+            contents: example.to_string(),
             idents: vec!["preview_changes".into(), "SourceTree".into()],
             ..Default::default()
         };
         for id in raw_line.idents.iter() {
             dbg!("loop", &id);
             if raw_line.should_be_modified(id) {
-                raw_line.contents_original = raw_line.clone().process_changes(&id).to_string();
-                dbg!(&raw_line.contents_original);
+                raw_line.contents = raw_line.clone().process_changes(&id).to_string();
+                dbg!(&raw_line.contents);
             }
         }
     }
@@ -433,3 +398,56 @@ mod tests {
     //     }
     // }
 }
+//     // Adjustment thread -> sends to builder thread
+//     let tsrsc = self.clone();
+//     let (tx_adj_2_build, rx_build) = mpsc::channel();
+//     let tx_adj_c = tx_adj_2_build.clone();
+//     let adjust_t = thread::spawn(move || tsrsc.make_adjustments(tx_adj_c));
+//
+//     // Builder thread -> sends to writer
+//     let (tx_build, rx_writer) = mpsc::channel();
+//
+//     // THE RX_BUILD gets values here, from tsrsc.make_adjustments(tx_c)
+//     let builder_t = thread::spawn(move || {
+//         let mut sf_hm: HashMap<String, Vec<AdjustedLine>> = HashMap::new();
+//         while let Ok((rl, filepath)) = rx_build.recv() {
+//             sf_hm.entry(filepath).and_modify(|v| v.push(rl.into()));
+//             dbg!("PUSH");
+//         }
+//         tx_build
+//             .send(sf_hm)
+//             .expect("unable to send complete sf_hm to writer!");
+//         dbg!("SF_HM GONE");
+//     });
+//
+//     if let Ok(_) = adjust_t.join() {
+//         drop(tx_adj_2_build);
+//     }
+//
+//     if let Ok(_) = builder_t.join() {
+//         // ALL THE LINES ARE IN SF_HM
+//     }
+//
+//     // THE WRITER thread is actually *this* one, the main one.
+//     while let Ok(sf_hm) = rx_writer.recv() {
+//         // sf_hm is a hashmap of filepath, adjusted_line
+//         dbg!(&sf_hm);
+//
+//         sf_hm.iter().for_each(|(sf_path, v)| {
+//             let mut output: Vec<&str> = Vec::new();
+//             for (e, _) in v.iter().enumerate() {
+//                 let current = &v[e];
+//                 println!("{}::{}", &current.line_num, &current.contents_original);
+//
+//                 output.push(&current.contents_original)
+//             }
+//
+//             if write_flag {
+//                 fs::write(&sf_path, output.join("\n"))
+//                     .expect("problem writing output to filepath")
+//             } else {
+//                 output.iter().for_each(|e| println!("{}", e));
+//             }
+//         });
+//     }
+// }
