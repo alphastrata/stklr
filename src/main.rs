f0baef7e0c44d7457f513daed71d63d9899c1f98
Jer <alphastrata@gmail.com>
2022-10-05 21:01:24 +0800

    chore: cleanup, cargo fmt, cargo fix, clippy

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,76 @@
+#![allow(non_snake_case)]
 //
 //! STKLR
 //
-
-#![allow(unused_imports)]
-use anyhow::Result;
-use log::{debug, error, info, trace, warn};
-use std::collections::HashMap;
 use STKLR::search::utils::SourceTree;
+use STKLR::termite;
 
-fn main() -> Result<()> {
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use log::debug;
+use std::collections::HashMap;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Minimum [`std`] prints.
+    #[arg(short, long, default_value_t = false)]
+    quiet: bool,
+
+    /// Turns on [`the`] [`log`] it's verbose -- not reccommended.
+    #[arg(short, long, default_value_t = false)]
+    debug: bool,
+
+    /// Show a report on % of stuff that's public etc.
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Generates a report, containing: x, y, z.
+    Report { path: Option<Vec<String>> },
+    /// [`Preview`] mode will print changes to [`std`] but not [`write`] anything to disk.
+    Preview { path: Option<Vec<String>> },
+    /// Run [`STKLR`] and [`write`] all changes found to disk.
+    Fix { path: Option<Vec<String>> },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    if cli.debug {
+        termite::setup_logger().unwrap();
+    }
+
+    _ = match &cli.command {
+        Commands::Report { path } => run_report(path),
+        Commands::Preview { path } => run(path, &cli, false),
+        Commands::Fix { path } => run(path, &cli, true),
+    };
+}
+// TODO: what even is this?
+#[allow(dead_code)]
+fn run_report(_paths: &Option<Vec<String>>) -> Result<()> {
+    Ok(())
+}
+//
+fn run(paths: &Option<Vec<String>>, cli: &Cli, write_mode: bool) -> Result<()> {
     let t1 = std::time::Instant::now();
 
-    //let st = SourceTree::new_from_dir("/media/jer/ARCHIVE/scrapers/rustwari");
-
-    let st = SourceTree::new_from_cwd();
+    let st = {
+        if let Some(paths) = paths {
+            SourceTree::new_from_paths(paths)
+        } else {
+            SourceTree::new_from_cwd()
+        }
+    };
 
     //NOTE: maybe par iter..?
     for rsc in st.source_files.iter() {
-        println!("Processing: {}", rsc.file.display());
+        if !cli.quiet {
+            println!("Processing: {}", rsc.file.display());
+        }
         let new_m = rsc
             .make_adjustments(&rsc.named_idents)
             .into_iter()
@@ -38,8 +91,17 @@ fn main() -> Result<()> {
             .collect::<Vec<String>>();
 
         // Write each file
-        _ = std::fs::write(&rsc.file, output.join("\n"));
-        println!("Completed in {}s\n", t1.elapsed().as_secs_f64())
+        if write_mode {
+            _ = std::fs::write(&rsc.file, output.join("\n"));
+            debug!("Write successful!");
+            if !cli.quiet {
+                output.iter().for_each(|i| println!("{i}"))
+            }
+        }
+
+        if !cli.quiet {
+            println!("Completed in {}s\n", t1.elapsed().as_secs_f64())
+        }
     }
 
     println!(
--- src/search/utils.rs
+++ src/search/utils.rs
@@ -1,10 +1,8 @@
 use super::consts::*;
-use crate::termite;
 
 use core::fmt::Display;
 use glob::glob;
-#[allow(unused_imports)]
-use log::{debug, error, info, trace, warn};
+use log::debug;
 use std::collections::HashMap;
 use std::fs;
 use std::hash::Hash;
@@ -71,27 +69,39 @@ impl SourceTree {
                 self.named_idents.push(e.to_string())
             });
         });
+        debug!("Idents populated.");
         self
     }
-    /// Creates a new SourceTree from the glob searching the current working directory the app is run
+    /// Creates a [`new`] [`SourceTree`] [`from`] a collection of [`path`] to source files.
+    pub fn new_from_paths(paths: &[String]) -> Self {
+        SourceTree {
+            source_files: paths
+                .iter()
+                .map(|p| RawSourceCode::new_from_file(p))
+                .collect(),
+            named_idents: Vec::new(),
+        }
+        .populate_idents()
+    }
+    /// Creates a [`new`] [`SourceTree`] [`from`] the [`glob`] [`search`] the current working directory the app is run
     /// in.
     pub fn new_from_cwd() -> Self {
-        Self::new_from_dir(".")
+        let path = std::env::current_dir().expect("Unable to ascertain current working directory, this is likely a permissions error with your OS.");
+
+        Self::new_from_dir(format!("{}", path.as_path().display()))
     }
-    /// Creates a new SourceTree from a given directory.
+    /// Creates a [`new`] [`SourceTree`] [`from`] a given directory.
     pub fn new_from_dir<P>(dir: P) -> Self
     where
         P: Display + AsRef<Path>,
     {
-        _ = termite::setup_logger().unwrap();
-
         let search_path = format!("{}/**/*.rs", dir);
         SourceTree {
             source_files: {
                 glob(&search_path)
                     .unwrap()
                     .filter_map(Result::ok)
-                    .filter(|f| !f.display().to_string().contains("target"))
+                    .filter(|f| !f.display().to_string().contains("target/"))
                     .map(|p| RawSourceCode::new_from_file(&p))
                     .collect::<Vec<RawSourceCode>>()
             },
@@ -99,9 +109,10 @@ impl SourceTree {
         }
         .populate_idents()
     }
-    /// Commits changes to disk, essentially writing the `Vec<AdjustedLine>` back to a file_path of
+    /// Commits changes to disk, essentially writing the [`AdjustedLine`] back to a [`path`] of
     /// the same name, line-by-line.
     pub fn write_changes(file: PathBuf, changes: &mut Vec<AdjustedLine>, write_flag: bool) {
+        debug!("SourceTree::write_changes was called");
         changes.sort_by(|a, b| a.line_num.cmp(&b.line_num));
         let output: Vec<String> = changes
             .into_iter()
@@ -109,7 +120,8 @@ impl SourceTree {
             .collect();
 
         if write_flag {
-            _ = fs::write(&file, output.join("\n")).unwrap(); //FIXME:
+            fs::write(&file, output.join("\n")).unwrap(); //FIXME:
+            debug!("Write successful.")
         } else {
             changes.iter().for_each(|e| println!("{}", e));
         }
@@ -179,7 +191,7 @@ impl RawSourceCode {
                         };
                         raw_line.find_docs();
                         raw_line.find_idents();
-                        if raw_line.idents.len() > 0 {
+                        if raw_line.idents.is_empty() {
                             raw_source_file
                                 .named_idents
                                 .extend(raw_line.idents.iter().cloned())
@@ -190,18 +202,18 @@ impl RawSourceCode {
         }
         raw_source_file.total_lines = raw_source_file.m.len();
         raw_source_file.named_idents.dedup();
-        raw_source_file.named_idents.retain(|x| x != ""); //TODO: can you initialise this better so avoid this?
+        raw_source_file.named_idents.retain(|x| !x.is_empty());
         raw_source_file
     }
 
-    /// Checks whether `self` [`should_be_modified`] and if so, [`process_changes`] from the passed
+    /// Checks whether `self` [`should_be_modified`] and if so, [`process`] [`from`] the passed
     /// `idents` is called.
     pub fn make_adjustments(&self, idents: &[String]) -> Vec<AdjustedLine> {
         self.m
             .iter()
             .filter(|(_, raw_line)| raw_line.should_be_modified(idents))
             .map(|(_, f)| f.clone().process_changes(idents))
-            .map(|rpl| rpl.clone().into())
+            .map(|rpl| rpl.into())
             .collect::<Vec<AdjustedLine>>()
     }
 }
@@ -209,7 +221,7 @@ impl RawSourceCode {
 //-------
 impl RawLine {
     /// Will return true for a SINGLE instance of when a modification should be made, how many may
-    /// really be in there is the domain of [`process_changes`]
+    /// really be in there is the domain of [`process`]
     fn should_be_modified(&self, idents: &[String]) -> bool {
         // NOTE: this isn't as bad as you'd initially think, you're out at the first branch if it's
         // not a docstring, or, out at the first 'hit'.
@@ -220,13 +232,14 @@ impl RawLine {
                     || self.contents.contains(&format!("{}.", i))
                     || self.contents.contains(&format!("{}'s", i)) && self.contents.contains("///")
                 {
+                    debug!("{i} will be modified.");
                     return true;
                 }
             }
         }
         false
     }
-    /// Actually processes the modifications to a RawLine's contents_modified
+    /// Actually [`process`] the modifications to a [`RawLine`] contents_modified
     fn process_changes(mut self, idents: &[String]) -> Self {
         for id in idents {
             let split_n_proc = &self
@@ -234,8 +247,10 @@ impl RawLine {
                 .split_whitespace()
                 .map(|sp| {
                     if sp.contains(id) {
+                        debug!("{} found in: {}", id, sp);
                         format!(" [`{}`]", id)
                     } else {
+                        debug!("No change to: {} ", sp);
                         format!(" {}", sp)
                     }
                 })
@@ -244,13 +259,13 @@ impl RawLine {
         }
         self
     }
-    /// Process a RawLine, by finding any idents, or docs that may be in there.
-    // NOTE: these are controlled by regexes in ./src/search/consts.rs
-    fn process(&mut self) {
-        self.find_docs();
-        self.find_idents();
-        self.idents.dedup();
-    }
+    // /// Process a [`RawLine`] by finding any idents, or docs that may be in there.
+    // // NOTE: these are controlled by regexes in ./src/search/consts.rs
+    // fn process(&mut self) {
+    //     self.find_docs();
+    //     self.find_idents();
+    //     self.idents.dedup();
+    // }
     fn find_idents(&mut self) {
         let text = self.contents.to_owned();
         macro_rules! generate_ident_find_loops {
@@ -259,6 +274,7 @@ impl RawLine {
                     if let Some(v) = caps.name("ident") {
                         let cap = v.as_str().to_string();
                         self.flavour = Flavour::Declare;
+                        debug!("{} added to caps.", cap);
                         self.idents.push(cap);
                     }
                 })*
@@ -275,7 +291,7 @@ impl RawLine {
         );
     }
 
-    /// Process preview_changes RawSourceCode [`find_docs`]
+    /// Process preview_changes [`RawSourceCode`] [`find_docs`]
     // NOTE: also regex controlled, see ./src/search/consts.rs
     fn find_docs(&mut self) {
         let text = self.contents.to_owned();
@@ -299,7 +315,7 @@ mod tests {
         let st = SourceTree::new_from_dir("/media/jer/ARCHIVE/scrapers/rustwari");
         for rsc in st.source_files.iter() {
             //rsc.make_adjustments(&rsc.named_idents);
-            info!("{}", rsc.file.display());
+            debug!("{}", rsc.file.display());
             let new_m = rsc
                 .make_adjustments(&rsc.named_idents)
                 .into_iter()
@@ -323,84 +339,85 @@ mod tests {
             _ = std::fs::write(&tmp_path, output.join("\n"));
         }
 
-        info!(
+        debug!(
             "{} FILES IN: {}s",
             st.source_files.len(),
             t1.elapsed().as_secs_f64()
         );
     }
-
-    // #[test]
-    // fn handle_imports() {
-    //     let example = r#"
-    //                     use anyhow::Result;
-    //                     use STKLR::search::utils::SourceTree;
-    //                     use STKLR::search::utils::RawSourceCode;
-    //                     }"#;
-    //
-    //     let mut raw_line = RawLine {
-    //         contents: example.into(),
-    //         ..Default::default()
-    //     };
-    //     raw_line.process();
-    //     assert_eq!(raw_line.idents.len(), 9);
-    // }
-    // #[test]
-    // fn handle_lifetime_annotations() {
-    //     let example = r#"async fn servitude<'a>(a: &'a str) -> bool{}"#;
-    //     let mut raw_line = RawLine {
-    //         contents: example.to_string(),
-    //         ..Default::default()
-    //     };
-    //     raw_line.process();
-    //     assert_eq!("servitude".to_string(), raw_line.idents[0])
-    // }
-    //#[test]
-    //fn handle_traits() {
-    //    let example = r#"pub trait Bless {
-    //    fn bless(&P) -> Blessing
-    //    where P: Clone + Send + Sync {
-    //    };
-    //    }"#;
-    //    let mut raw_line = RawLine {
-    //        contents: example.into(),
-    //        ..Default::default()
-    //    };
-    //    raw_line.process();
-    //    assert_eq!(raw_line.idents[1], "Bless");
-    //    assert_eq!(raw_line.idents[0], "bless");
-    //}
-    //#[test]
-    //fn multi_matches_single_line() {
-    //    let example =
-    //        r#"a preview_changes to be linked, and another preview_changes here linked too."#;
-    //    let raw_line = RawLine {
-    //        contents: example.to_string(),
-    //        idents: vec!["preview_changes".into()],
-    //        ..Default::default()
-    //    };
-
-    //    let expected =
-    //        "a [`preview_changes`] to be linked, and another [`preview_changes`] here linked too.";
-    //}
-    // #[test]
-    // fn fullstop_after_ident() {
-    //     let example = r#"a preview_changes to the mighty SourceTree."#;
-    //     let mut raw_line = RawLine {
-    //         contents: example.to_string(),
-    //         idents: vec!["preview_changes".into(), "SourceTree".into()],
-    //         ..Default::default()
-    //     };
-    // }
-    // #[test]
-    // fn ident_has_apos_s() {}
-    // fn show_matched_idents() {
-    //     for path in glob("./**/*.rs").unwrap().filter_map(Result::ok) {
-    //         let p = path;
-    //         let rsc = RawSourceCode::new_from_file(&p);
-    //         if !rsc.named_idents.is_empty() {
-    //             dbg!(rsc.named_idents);
-    //         }
-    //     }
-    // }
 }
+
+// DEAD TESTS.
+// #[test]
+// fn handle_imports() {
+//     let example = r#"
+//                     use anyhow::Result;
+//                     use STKLR::search::utils::SourceTree;
+//                     use STKLR::search::utils::RawSourceCode;
+//                     }"#;
+//
+//     let mut raw_line = RawLine {
+//         contents: example.into(),
+//         ..Default::default()
+//     };
+//     raw_line.process();
+//     assert_eq!(raw_line.idents.len(), 9);
+// }
+// #[test]
+// fn handle_lifetime_annotations() {
+//     let example = r#"async fn servitude<'a>(a: &'a str) -> bool{}"#;
+//     let mut raw_line = RawLine {
+//         contents: example.to_string(),
+//         ..Default::default()
+//     };
+//     raw_line.process();
+//     assert_eq!("servitude".to_string(), raw_line.idents[0])
+// }
+//#[test]
+//fn handle_traits() {
+//    let example = r#"pub trait Bless {
+//    fn bless(&P) -> Blessing
+//    where P: Clone + Send + Sync {
+//    };
+//    }"#;
+//    let mut raw_line = RawLine {
+//        contents: example.into(),
+//        ..Default::default()
+//    };
+//    raw_line.process();
+//    assert_eq!(raw_line.idents[1], "Bless");
+//    assert_eq!(raw_line.idents[0], "bless");
+//}
+//#[test]
+//fn multi_matches_single_line() {
+//    let example =
+//        r#"a preview_changes to be linked, and another preview_changes here linked too."#;
+//    let raw_line = RawLine {
+//        contents: example.to_string(),
+//        idents: vec!["preview_changes".into()],
+//        ..Default::default()
+//    };
+
+//    let expected =
+//        "a [`preview_changes`] to be linked, and another [`preview_changes`] here linked too.";
+//}
+// #[test]
+// fn fullstop_after_ident() {
+//     let example = r#"a preview_changes to the mighty SourceTree."#;
+//     let mut raw_line = RawLine {
+//         contents: example.to_string(),
+//         idents: vec!["preview_changes".into(), "SourceTree".into()],
+//         ..Default::default()
+//     };
+// }
+// #[test]
+// fn ident_has_apos_s() {}
+// fn show_matched_idents() {
+//     for path in glob("./**/*.rs").unwrap().filter_map(Result::ok) {
+//         let p = path;
+//         let rsc = RawSourceCode::new_from_file(&p);
+//         if !rsc.named_idents.is_empty() {
+//             dbg!(rsc.named_idents);
+//         }
+//     }
+// }
