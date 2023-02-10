cee8df6afed3fbd06828c533b7d91e6156cb7a18
jer <alphastrata@gmail.com>
2023-02-10 22:52:44 +1100

    play: RwLocks in parking lot are kinda cool...

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
+#![allow(non_snake_case)]
+#![allow(unused_must_use)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
 use anyhow::Result;
 use glob::glob;
 use std::collections::HashMap;
 use std::path::Path;
+
+/// Gives you a wrapped Arc<RwLock<Node>>
+#[macro_export]
+macro_rules! quick_node {
+    ($val:expr) => {{
+        let node = Node::new($val.to_string());
+        Arc::new(RwLock::new(node))
+    }};
+}
+
+#[macro_export]
+/// Unwraps unsafely a .read() lock
+macro_rules! open {
+    ($e:expr) => {{
+        let inner = $e.read().unwrap();
+        *inner
+    }};
+}
+
+#[macro_export]
+/// Unsafely unwraps a .write() lock
+macro_rules! take {
+    ($x:expr) => {
+        (*$x.write().unwrap())
+    };
+}
 
 #[derive(Debug)]
 struct SourceCode {
